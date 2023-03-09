fa171a0307c21c24050ffaba79e418caf6829b1f
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2023-03-09 19:18:30 +0100

    phone test 2

--- scripts/index.js
+++ scripts/index.js
@@ -8,20 +8,35 @@ let allUsers = JSON.parse(window.localStorage.getItem("allUsers"))
 let allNations = JSON.parse(window.localStorage.getItem("allNations"))
 let authorized = userData ? true : false
 
-sendGSRequest("users", "getData", {}, (data) => {
-    window.localStorage.setItem("allUsers", JSON.stringify(data))
-    allUsers = data
-    if (authorized) { // Если авторизован то обновляем информацию о пользователе
-        window.localStorage.setItem("userData", JSON.stringify(data[userData.id]))
-        userData = data[userData.id]
-    }
+try {
+    sendGSRequest("users", "getData", {}, (data) => {
+        try {
+            window.localStorage.setItem("allUsers", JSON.stringify(data))
+            allUsers = data
+            if (authorized) { // Если авторизован то обновляем информацию о пользователе
+                window.localStorage.setItem("userData", JSON.stringify(data[userData.id]))
+                userData = data[userData.id]
+            }
+        
+            $(".userData").text(JSON.stringify(userData))
+            $(".allUsers").text(JSON.stringify(allUsers))
+        } catch (error) {
+            $(".error").text(error)
+        }
+        
+    })
+    
+    sendGSRequest("nations", "getData", {}, (data) => {
+        try {
+            window.localStorage.setItem("allNations", JSON.stringify(data))
+            allNations = data
+            $(".allNations").text(JSON.stringify(allNations))
+        } catch (error) {
+            $(".error").text(error)
+        }
+        
+    })
+} catch (error) {
+    $(".error").text(error)
+}
 
-    $(".userData").text(JSON.stringify(userData))
-    $(".allUsers").text(JSON.stringify(allUsers))
-})
-
-sendGSRequest("nations", "getData", {}, (data) => {
-    window.localStorage.setItem("allNations", JSON.stringify(data))
-    allNations = data
-    $(".allNations").text(JSON.stringify(allNations))
-})
