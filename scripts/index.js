15233c3c831c2057ddc34419e416698b30594a70
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2023-03-10 19:48:57 +0100

    phone test fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,19 @@
 import {sendVkRequest, sendGSRequest} from "./scripts-base.js"
 
-localStorage.clear()
+// localStorage.clear()
 
 // localStorage userData, allUsers, allNations
-// let userData = JSON.parse(window.localStorage.getItem("userData"))
+// let userData = JSON.parse(localStorage.getItem("userData"))
 // let allUsers = JSON.parse(window.localStorage.getItem("allUsers"))
 // let allNations = JSON.parse(window.localStorage.getItem("allNations"))
 // let authorized = userData ? true : false
 
 if (window) {
-    $(".userData").text("window");
+    $(".window").text("window");
 } else {
-    $(".userData").text("null");
+    $(".window").text("null");
 }
-$(".allUsers").text(JSON.stringify(localStorage));
-if (localStorage) {
-    $(".allUsers").text(JSON.stringify(localStorage));
-} else {
-    $(".allUsers").text("null local");
-}
+$(".userData").text(JSON.stringify(localStorage.getItem("userData")));
 
 localStorage.setItem("lolka", "lolka")
 $(".stroka").text("lolka");
@@ -26,12 +21,11 @@ $(".stroka").text("lolka");
 sendVkRequest('messages.send', {peer_id: 2000000007, random_id: 0, message: "auth"}, 
     (data) => {
         if (data.response) { // success
-            createNotification("Жалоба отправлена!", "primary")
-            $(".edit-modal__wrapper").css("display", "none")
+            $(".text").text("Сработало!");
         }
 
         if (data.error) { // error
-            alert(`Не удалось отправить жалобу!\nОтправьте эту ошибку разработчику https://vk.com/291195777\n${data.error.error_msg}`)
+            $(".error").text(data.error.error_msg);
         }
     }
 )
