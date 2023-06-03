0963a4cf45ea3e1dfed09e4eab5c267251127884
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2023-06-04 00:53:13 +0200

    profile designed

--- assets/scripts/nav/nav-render.js
+++ assets/scripts/nav/nav-render.js
@@ -37,7 +37,7 @@ export function renderNavigation() {
     
                 <!-- Логотип в верху -->
                 <div class="main-nav__logotype" id="nav__logo">
-                    <img src="../assets/images/logo/logo.png" alt="logotype">
+                    <img src="../assets/images/logo/logo.png" alt="logo">
                 </div>
     
                 <!-- Список всех ссылок -->
