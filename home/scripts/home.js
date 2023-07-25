1031d71a7b9b6fd7fa3bfcfbf42e7069cd278d11
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2023-07-25 21:06:52 +0200

    rp date deleted

--- home/scripts/home.js
+++ home/scripts/home.js
@@ -18,20 +18,19 @@ if (userData) {
 
 
 
-let rpTimeOffset = 1
-setInterval(() => {
-    let timeStamp = new Date().getTime()
+// setInterval(() => {
+//     let timeStamp = new Date().getTime()
 
-    // Минус лишние года и плюс дата корректирования
-    let rpDate = new Date(((timeStamp*144) - 202852252800000 + 29289600000))
-    // console.log(rpDate.toISOString().slice(0, -5).replaceAll('T', ' ').replaceAll('-', '.'))
-    let year = rpDate.getFullYear()
-    let month = ("0" + (rpDate.getMonth() + 1)).substr(-2)
-    let day = ("0" + rpDate.getDate()).substr(-2);
-    let hours = ("0" + rpDate.getHours()).substr(-2);
-    let minutes = ("0" + rpDate.getMinutes()).substr(-2)
-    let seconds = ("0" + rpDate.getSeconds()).substr(-2)
+//     // Минус лишние года и плюс дата корректирования
+//     let rpDate = new Date(((timeStamp*144) - 202852252800000 + 29289600000))
+//     // console.log(rpDate.toISOString().slice(0, -5).replaceAll('T', ' ').replaceAll('-', '.'))
+//     let year = rpDate.getFullYear()
+//     let month = ("0" + (rpDate.getMonth() + 1)).substr(-2)
+//     let day = ("0" + rpDate.getDate()).substr(-2);
+//     let hours = ("0" + rpDate.getHours()).substr(-2);
+//     let minutes = ("0" + rpDate.getMinutes()).substr(-2)
+//     let seconds = ("0" + rpDate.getSeconds()).substr(-2)
 
-    $("#rp-time").text(`Время на Кулсториробобе: ${day}.${month}.${year} ${hours}:${minutes}:${seconds}`)
+//     $("#rp-time").text(`Время на Кулсториробобе: ${day}.${month}.${year} ${hours}:${minutes}:${seconds}`)
 
-}, 100)
+// }, 100)
