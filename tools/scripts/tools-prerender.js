098fb30606c97f90e15a6188f2df4bcb1bbdee77
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2023-07-25 21:34:41 +0200

    delete relocate

--- tools/scripts/tools-prerender.js
+++ tools/scripts/tools-prerender.js
@@ -1,9 +1,2 @@
 import { relocate } from "../../assets/scripts/global-functions.js"
 
-$("#tool-vk-words-rating").on("click tap", () => {
-    relocate("../tool-vk-words-rating/index.html")
-})
-
-// $("#tool-vk-top-active").on("click tap", () => {
-//     relocate("../tool-vk-top-active/index.html")
-// })
