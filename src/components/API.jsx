f89ff11f23571dfbcdbce6e8fb0cefc98b95107a
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2024-05-02 12:34:51 +0200

    new dot-env

--- src/components/API.jsx
+++ src/components/API.jsx
@@ -1,11 +1,12 @@
-import { GoogleAPI, GoogleAPIdev, VkToken } from "./DOT-ENV"
 import $ from "jquery";
+
+const GOOGLE_API = "https://script.google.com/macros/s/AKfycby0ajEKJKjRzOIYqPIgnKW8nl2v2er66Tcmz8dVMe1ENdIoj_kFff3gYH2-KAm7y-jp4Q/exec"
 
 // Отправить запрос
 export function GSAPI(action, data={}, func) {
     $.ajax({
         crossDomain: true,
-        url: GoogleAPI + "?action=" + action,
+        url: GOOGLE_API + "?action=" + action,
         method: "GET",
         dataType: "JSONP",
         data: data,
@@ -17,14 +18,14 @@ export function GSAPI(action, data={}, func) {
 // Получить ссылку на вк метод
 function getMethodUrl(method, params, token) {
     params = params || {}
-    params["access_token"] = "vk1.a." + token
+    params["access_token"] = token
     params["v"] = "5.131"
     return "https://api.vk.com/method/" + method + "?" + $.param(params)
 }
 
 
 // Отправить запрос
-export function VKAPI(method, params, func=null, token=VkToken) {
+export function VKAPI(method, params, func=null, token=process.env.REACT_APP_VK_TOKEN) {
     $.ajax({
         url: getMethodUrl(method, params, token),
         method: "GET",
