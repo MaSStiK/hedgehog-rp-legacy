c9d6daba817221a47ade21a6a55d8ce7970772bc
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2024-08-05 12:09:45 +0200

    s4e6

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -56,7 +56,7 @@ export default function HomePage() {
 
                 {/* <iframe width="520" height="280" src="https://www.youtube.com/embed/1yeE1jlYFnc?si=wsqkPsAl4VAA1FZm" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe> */}
                 
-                <Link to="https://tv.hedgehog-rp.ru/watch/s4e5">
+                <Link to="https://tv.hedgehog-rp.ru/watch/s4e6">
                     <img className="home__thumbnail" src={imgThumbnail} alt="thumbnail" />
                     <h3>4 сезон 5 серия | Коллапс</h3>
                 </Link>
