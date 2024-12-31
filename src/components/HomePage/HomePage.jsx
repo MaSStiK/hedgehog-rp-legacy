9935ad65875d93999e79875339a52e08045cbae2
MaSStiK <mathew.ovchinnikov@gmail.com>
2024-12-31 17:59:34 +0100

    s4e12

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -56,9 +56,9 @@ export default function HomePage() {
 
                 {/* <iframe width="520" height="280" src="https://www.youtube.com/embed/1yeE1jlYFnc?si=wsqkPsAl4VAA1FZm" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe> */}
                 
-                <Link to="https://tv.hedgehog-rp.ru/watch/s4e8">
+                <Link to="https://tv.hedgehog-rp.ru/watch/s4e12">
                     <img className="home__thumbnail" src={imgThumbnail} alt="thumbnail" />
-                    <h3>4 сезон 8 серия | Суета</h3>
+                    <h3>4 сезон 12 серия | Новогодние истории</h3>
                 </Link>
                 
                 <div className="flex-row flex-wrap">
